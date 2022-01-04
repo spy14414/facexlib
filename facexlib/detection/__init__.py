6be8bdcfe32198374c17588132a72022ad88ae3b
spy14414 <17865190816@163.com>
2022-01-04 17:26:23 +0800

    upload
    model_path

--- facexlib/detection/__init__.py
+++ facexlib/detection/__init__.py
@@ -8,14 +8,17 @@ from .retinaface import RetinaFace
 def init_detection_model(model_name, half=False, device='cuda'):
     if model_name == 'retinaface_resnet50':
         model = RetinaFace(network_name='resnet50', half=half)
-        model_url = 'https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_Resnet50_Final.pth'
+        model_url = 'experiments/pretrained_models/detection_Resnet50_Final.pth'
     elif model_name == 'retinaface_mobile0.25':
         model = RetinaFace(network_name='mobile0.25', half=half)
         model_url = 'https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_mobilenet0.25_Final.pth'
     else:
         raise NotImplementedError(f'{model_name} is not implemented.')
 
-    model_path = load_file_from_url(url=model_url, model_dir='facexlib/weights', progress=True, file_name=None)
+    if model_url.startswith('https://'):
+        model_path = load_file_from_url(url=model_url, model_dir='facexlib/weights', progress=True, file_name=None)
+    else:
+        model_path = model_url
     # TODO: clean pretrained model
     load_net = torch.load(model_path, map_location=lambda storage, loc: storage)
     # remove unnecessary 'module.'
